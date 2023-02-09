9080289632fdecfcd07b7c13fa46abc77410d9ca
Matéo <mateomesguen@gmail.com>
2023-02-09 08:08:54 +0100

    adding artistHandler, with some function like nameList

---
+++ src/artistHandler.go
@@ -0,0 +1,99 @@
+package src
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type data struct {
+	Artists   string
+	Locations string
+	Date      string
+	Relation  string
+}
+
+type groupe struct {
+	Id           int
+	Image        string
+	Name         string
+	Members      []string
+	CreationDate int
+	FirstAlbum   string
+	locations    string
+	ConcertDates string
+	Relations    string
+}
+
+func main() {
+	art()
+	loc()
+	namelist()
+	locatlist()
+}
+
+func art() {
+	var d data
+	url := "https://groupietrackers.herokuapp.com/api"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal([]byte(body), &d)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	fmt.Println(d.Artists)
+}
+
+func loc() {
+	var d data
+	url := "https://groupietrackers.herokuapp.com/api"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal([]byte(body), &d)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	fmt.Println(d.Locations)
+}
+
+func namelist() {
+	var g []groupe
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal([]byte(body), &g)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	for i := 0; i < len(g); i++ {
+		fmt.Println("Groupe :", i, g[i].Name)
+		fmt.Println(g[i].Members)
+	}
+}
+
+func locatlist() {
+	var g []groupe
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal([]byte(body), &g)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	for i := 0; i < len(g); i++ {
+		fmt.Println(g[i].locations)
+	}
+}
