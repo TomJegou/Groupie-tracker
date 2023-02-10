a05c262cf273e311f053976f4bfb86a822499874
TomJegou <tomjegou10@gmail.com>
2023-02-10 10:38:00 +0100

    moving stucts + creating global variables + global const

--- src/artistHandler.go
+++ src/artistHandler.go
@@ -7,33 +7,6 @@ import (
 	"text/template"
 )
 
-type Artists struct {
-	Id           int
-	Image        string
-	Name         string
-	Members      []string
-	CreationDate int
-	FirstAlbum   string
-	Locations    []string
-	ConcertDates string
-	Relations    string
-}
-
-type Location struct {
-	Id        int
-	Locations []string
-}
-
-type Date struct {
-	Id    int
-	Dates []string
-}
-
-type Relation struct {
-	Id             int
-	DatesLocations map[string][]string
-}
-
 func getApi(url string) string {
 	req, _ := http.NewRequest("GET", url, nil)
 	res, errors := http.DefaultClient.Do(req)
--- src/server.go
+++ src/server.go
@@ -7,10 +7,47 @@ import (
 	"text/template"
 )
 
+const URLARTISTS = "https://groupietrackers.herokuapp.com/api/artists"
+const URLDATES = "https://groupietrackers.herokuapp.com/api/dates"
+const URLLOCATIONS = "https://groupietrackers.herokuapp.com/api/locations"
+const URLRELATION = "https://groupietrackers.herokuapp.com/api/relation"
+
+type Artist struct {
+	Id           int
+	Image        string
+	Name         string
+	Members      []string
+	CreationDate int
+	FirstAlbum   string
+	Locations    []string
+	ConcertDates string
+	Relations    string
+}
+
+type Location struct {
+	Id        int
+	Locations []string
+}
+
+type Date struct {
+	Id    int
+	Dates []string
+}
+
+type Relation struct {
+	Id             int
+	DatesLocations map[string][]string
+}
+
 func Accueil(w http.ResponseWriter, r *http.Request) {
 	template, _ := template.ParseFiles("static/html/index.html")
 	template.Execute(w, nil)
 }
+
+var Artists []Artist
+var Dates map[string][]Date
+var Locations map[string][]Location
+var Relations map[string][]Relation
 
 func StartServer() {
 	FileServer := http.FileServer(http.Dir("./static"))
