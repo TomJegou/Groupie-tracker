7596b1180575be689d2b2e0257c0a68eda1c80e3
TomJegou <tomjegou10@gmail.com>
2023-02-10 11:20:35 +0100

    renaming

--- src/libraryArtist.go
+++ src/libraryArtist.go
@@ -6,6 +6,6 @@ import (
 )
 
 func ArtistsHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	template, _ := template.ParseFiles("static/html/artists.html")
+	template, _ := template.ParseFiles("static/html/libraryArtists.html")
 	template.Execute(w, nil)
 }
