eb6646be25906e775fa8d203655a97b6c4b7e861
L0uxe <loulourxl@hotmail.com>
2023-02-12 19:43:04 +0100

    PRINT ALL CARD ON ARTIST PAGE

--- src/libraryArtists.go
+++ src/libraryArtists.go
@@ -6,6 +6,6 @@ import (
 )
 
 func libraryArtists(w http.ResponseWriter, r *http.Request) {
-	template, _ := template.ParseFiles("static/html/libraryArtists.html")
-	template.Execute(w, nil)
+	template, _ := template.ParseFiles("static/html/libraryArtists.tmpl")
+	template.Execute(w, Artists)
 }
