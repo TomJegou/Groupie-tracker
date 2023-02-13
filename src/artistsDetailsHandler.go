36101a3ee4cfb27f47b5dc73a9cd5e23dcbad0ee
TomJegou <tomjegou10@gmail.com>
2023-02-13 09:19:05 +0100

    transforming libartist into form

--- src/artistsDetailsHandler.go
+++ src/artistsDetailsHandler.go
@@ -1,11 +1,13 @@
 package src
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
 
 func ArtistsDetailsHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.FormValue("artistCard"))
 	template, _ := template.ParseFiles("static/html/artistsDetails.html")
 	template.Execute(w, nil)
 }
