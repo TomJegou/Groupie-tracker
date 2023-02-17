e0f8fbf7c1af15f441a2d02f6814b887766d344a
Simon <simon.louis638@gmail.com>
2023-02-17 09:20:17 +0100

    adding searchbar

--- src/libraryArtists.go
+++ src/libraryArtists.go
@@ -3,14 +3,42 @@ package src
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
-func libraryArtists(w http.ResponseWriter, r *http.Request) {
-	template, errors := template.ParseFiles("static/html/libraryArtists.html")
-	if errors != nil {
-		fmt.Println("Error Parsing Template")
-		fmt.Println(errors)
+func removeArtist(artistList []Artist, index int) []Artist {
+	artistList[index] = artistList[len(artistList)-1]
+	return artistList[:len(artistList)-1]
+}
+
+func searchArtists(artistList []Artist, searchContent string) []Artist {
+	for indexChar, char := range searchContent {
+		for _, artist := range artistList {
+			if strings.ToLower(string(artist.Name[indexChar])) != strings.ToLower(string(char)) {
+				artistList = removeArtist(artistList, indexChar)
+			}
+		}
 	}
-	template.Execute(w, Artists)
+	return artistList
+}
+
+func libraryArtists(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		template, errors := template.ParseFiles("static/html/libraryArtists.html")
+		if errors != nil {
+			fmt.Println("Error Parsing Template")
+			fmt.Println(errors)
+		}
+		template.Execute(w, Artists)
+	} else if r.Method == "POST" {
+		searchContent := r.FormValue("searchBar")
+		Artists = searchArtists(Artists, searchContent)
+		template, errors := template.ParseFiles("static/html/libraryArtists.html")
+		if errors != nil {
+			fmt.Println("Error Parsing Template")
+			fmt.Println(errors)
+		}
+		template.Execute(w, Artists)
+	}
 }
