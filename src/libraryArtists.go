8ab8615c32b13214c2f2d6b391a8a398d8504a32
TomJegou <tomjegou10@gmail.com>
2023-02-17 11:57:22 +0100

    refactoring func search

--- src/libraryArtists.go
+++ src/libraryArtists.go
@@ -8,17 +8,21 @@ import (
 )
 
 func searchArtists(artistList []Artist, artistListResult []Artist, searchContent string) {
-	for indexChar, char := range searchContent {
-		for _, artist := range artistList {
-			if strings.EqualFold(string(artist.Name[indexChar]), strings.ToLower(string(char))) {
-				artistListResult = append(artistListResult, artist)
+	for _, artist := range artistList {
+		isOk := true
+		for indexChar, char := range searchContent {
+			if !strings.EqualFold(string(artist.Name[indexChar]), string(char)) {
+				isOk = false
+				break
 			}
+		}
+		if isOk {
+			artistListResult = append(artistListResult, artist)
 		}
 	}
 }
 
 func libraryArtists(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method)
 	if r.Method == "GET" {
 		template, errors := template.ParseFiles("static/html/libraryArtists.html")
 		if errors != nil {
