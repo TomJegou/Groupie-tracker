a5a8a80623d8d34ee8afbb6338d3db7005246171
L0uxe <loulourxl@hotmail.com>
2023-02-19 16:42:37 +0100

    start testing to add 3d object

---
+++ src/scripts/script3d.js
@@ -0,0 +1,8 @@
+const sceneContainer = document.getElementById('scene-container');
+const renderer = new THREE.WebGLRenderer();
+sceneContainer.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera()
+const ambientLight = new THREE.AmbientLight(;
