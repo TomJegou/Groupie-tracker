ab2c746f292a849b6de56962aaf26c7b1909b0e1
TomJegou <tomjegou10@gmail.com>
2023-02-24 12:30:24 +0100

    close parentheses

--- src/scripts/script3d.js
+++ src/scripts/script3d.js
@@ -5,4 +5,4 @@ sceneContainer.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera()
-const ambientLight = new THREE.AmbientLight(;
+const ambientLight = new THREE.AmbientLight();
