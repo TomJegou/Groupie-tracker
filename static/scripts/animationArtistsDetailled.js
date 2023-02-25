f864e89634a7ea63437bd8a198edab24b9c03d0a
TomJegou <tomjegou10@gmail.com>
2023-02-25 19:49:01 +0100

    js

---
+++ static/scripts/animationArtistsDetailled.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    swipeTop();
+  };
+
+function swipeTop(){
+    var image = document.getElementById("idImgArtistDetailled");
+    image.style.position = "absolute";
+    image.style.top = "0px";
+    image.style.left = "0px";
+}
