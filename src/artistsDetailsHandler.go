378fecd179211a6c0016cb8e129d70a986c1e9c3
TomJegou <tomjegou10@gmail.com>
2023-03-02 21:36:56 +0100

    add comments

--- src/artistsDetailsHandler.go
+++ src/artistsDetailsHandler.go
@@ -7,21 +7,30 @@ import (
 	"sync"
 )
 
+/*Structures*/
+
 type ArtistDetailled struct {
 	*Artist
 	ArtistConcertsDatesLocation map[string][]string
 }
 
-func findArtistById(listArtist []Artist, id int) {
-	for _, artist := range listArtist {
+/*Functions*/
+
+/*
+Find the artist who as the same id as the id passed as parameter
+from the Artists slice
+*/
+func findArtistById(id int) {
+	for _, artist := range Artists {
 		if artist.Id == id {
 			ChanArtDet <- artist
 			return
 		}
 	}
-	ChanArtDet <- listArtist[0]
+	ChanArtDet <- Artists[0]
 }
 
+/*Artist detailled page's handler*/
 func ArtistsDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	OnLibraryArtists = false
 	if len(Artists) == 0 {
@@ -41,7 +50,7 @@ func ArtistsDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		go ParseHtml("static/html/artistsDetails.html")
 		template := <-ChanTemplates
-		go findArtistById(Artists, idArtist)
+		go findArtistById(idArtist)
 		artist := <-ChanArtDet
 		artistDetailled := ArtistDetailled{Artist: &artist, ArtistConcertsDatesLocation: Relations["index"][idArtist-1].DatesLocations}
 		template.Execute(w, artistDetailled)
