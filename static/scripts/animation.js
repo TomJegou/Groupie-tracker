9307cede2cf39644c2fa5190473125290e51c93d
L0uxe <loulourxl@hotmail.com>
2023-03-03 09:25:07 +0100

    wip dropdown for date artist

--- static/scripts/animation.js
+++ static/scripts/animation.js
@@ -1,2 +1,15 @@
-function dropdownFunction() {
+let dropdownBtn = document.getElementsByClassName("btn");
+let dropdownMenu = document.getElementsByClassName("dropdown");
+let toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+};
+//open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function () {
+  toggleDropdown();
+});
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
   }
+});
