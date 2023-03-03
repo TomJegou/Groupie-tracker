95936265a18c4ecbb0d1a508338f20040984cf5b
L0uxe <loulourxl@hotmail.com>
2023-03-03 09:46:12 +0100

    wip dropdown menu

--- static/scripts/animation.js
+++ static/scripts/animation.js
@@ -1,15 +1,13 @@
-let dropdownBtn = document.getElementsByClassName("btn");
-let dropdownMenu = document.getElementsByClassName("dropdown");
-let toggleDropdown = function () {
-  dropdownMenu.classList.toggle("show");
-};
+
+function toggleDropdown() {
+  document.getElementsByClassName("dropdown").classList.toggle("show");
+}
 //open/close when dropdown button is clicked
-dropdownBtn.addEventListener("click", function () {
-  toggleDropdown();
-});
+document.getElementById("btn").addEventListener("click",function(){ toggleDropdown()});
 // Close dropdown when dom element is clicked
 document.documentElement.addEventListener("click", function () {
-  if (dropdownMenu.classList.contains("show")) {
+  if (document.getElementsByClassName("dropdown").classList.contains("show")) {
     toggleDropdown();
   }
 });
+
