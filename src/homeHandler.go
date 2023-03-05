eea89b4ffa81196f7f3c2ae7b0e74649a177901b
TomJegou <tomjegou10@gmail.com>
2023-03-05 13:14:20 +0100

    ref

--- src/homeHandler.go
+++ src/homeHandler.go
@@ -5,17 +5,9 @@ import (
 	"net/http"
 )
 
-func GetIP(r *http.Request) string {
-	forwarded := r.Header.Get("X-FORWARDED-FOR")
-	if forwarded != "" {
-		return forwarded
-	}
-	return r.RemoteAddr
-}
-
 /*Home page's handler*/
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(GetIP(r))
+	fmt.Println(r.Host)
 	OnLibraryArtists = false
 	go ParseHtml("static/html/index.html")
 	template := <-ChanTemplates
