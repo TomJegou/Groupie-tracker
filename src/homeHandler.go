c29d571824a9f9ef1957b3da00d23a323a0c7fc1
TomJegou <tomjegou10@gmail.com>
2023-03-05 13:19:48 +0100

    ref

--- src/homeHandler.go
+++ src/homeHandler.go
@@ -1,13 +1,12 @@
 package src
 
 import (
-	"fmt"
 	"net/http"
 )
 
 /*Home page's handler*/
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
+	ListeningAddr.Ipv4 = r.Host
 	OnLibraryArtists = false
 	go ParseHtml("static/html/index.html")
 	template := <-ChanTemplates
