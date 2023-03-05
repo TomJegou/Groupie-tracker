fee2c777096293c7487cdca619da69765224b79b
TomJegou <tomjegou10@gmail.com>
2023-03-05 13:27:23 +0100

    remove useless print

--- src/server.go
+++ src/server.go
@@ -118,7 +118,6 @@ func StartServer(wg *sync.WaitGroup) {
 	http.HandleFunc("/artistsDetails", ArtistsDetailsHandler)
 	http.HandleFunc("/about", AboutHandler)
 	http.HandleFunc("/legalNotice", LegalNoticeHandler)
-	fmt.Println("http://" + ListeningAddr.Ipv4 + ":" + ListeningAddr.Port)
 	err := http.ListenAndServe(ListeningAddr.Ipv4+":"+ListeningAddr.Port, nil)
 	if err != nil {
 		fmt.Println(err)
