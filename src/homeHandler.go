07c8fb9ae73f23f04d0c4be4b6de1778d7b5f1a6
TomJegou <tomjegou10@gmail.com>
2023-03-05 13:04:08 +0100

    add getip func

--- src/homeHandler.go
+++ src/homeHandler.go
@@ -1,11 +1,21 @@
 package src
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func GetIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
+}
+
 /*Home page's handler*/
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(GetIP(r))
 	OnLibraryArtists = false
 	go ParseHtml("static/html/index.html")
 	template := <-ChanTemplates
