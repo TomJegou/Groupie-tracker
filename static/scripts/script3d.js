307fe232f755af4a61a51429ba192267585e60a8
L0uxe <loulourxl@hotmail.com>
2023-03-06 10:53:57 +0100

    wip

--- static/scripts/script3d.js
+++ static/scripts/script3d.js
@@ -1,41 +1,45 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene ()
-const loader = new GLTFLoader ( )
-loader.load('http://localhost/static/img/casque.gltf',function(gltf){
-    console.log(gltf)
-    const root = gltf.scene;
-    root.scale.set(0.05,0.05,0.05)
-    scene.add(root);
-},function(error){
-    console.log("An error occurred :",error)
-},)
 
-const light = new THREE.DirectionalLight(0xffffff,1)
-light.position.set(2,2,5)
-scene.add(light)
+function init(){
+    const canvas = document.querySelector('.webgl')
+    const scene = new THREE.Scene ()
+    const loader = new GLTFLoader ( )
+    loader.load('http://localhost/static/img/casque.gltf',function(gltf){
+        console.log(gltf)
+        const root = gltf.scene;
+        root.scale.set(0.05,0.05,0.05)
+        scene.add(root);
+    },function(error){
+        console.log("An error occurred :",error)
+    },)
 
-const sizes ={
-    width: window.innerWidth,
-    height: window.innerHeight
+    const light = new THREE.DirectionalLight(0xffffff,1)
+    light.position.set(2,2,5)
+    scene.add(light)
+
+    const sizes ={
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+
+    const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1,100)
+    camera.position.set(0,1,2)
+    scene.add(camera)
+
+    const renderer = new THREE.WebGL1Renderer({
+        canvas: canvas
+    })
+    camera.position.set(-50, 40, 350);
+    renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+    renderer.setSize(container.clientWidth,container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio)
+    container.appendChild(renderer.domElement);
+    renderer.render(scene.camera)
 }
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1,100)
-camera.position.set(0,1,2)
-scene.add(camera)
-
-const renderer = new THREE.WebGL1Renderer({
-    canvas: canvas
-})
-camera.position.set(-50, 40, 350);
-renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
-renderer.setSize(container.clientWidth,container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio)
-container.appendChild(renderer.domElement);
-renderer.render(scene.camera)
-function animate(){
-}
+// function animate(){
+// }
 
 
-animate()
+// animate()
+init()
