479864abcc6505a8b42dc70611a95705e79c7c88
L0uxe <loulourxl@hotmail.com>
2023-03-07 22:47:10 +0100

    wipdropdown

--- static/scripts/animation.js
+++ static/scripts/animation.js
@@ -1,12 +1,10 @@
-function show() {
+function dropdown() {
   var container = document.getElementById("dropdown-container");
-
   if (container.style.display == "block") {
     container.style.display = "none";
   } else {
     container.style.display = "block";
   }
-
 window.onclick = function (event) {
   if (!event.target.matches('.dropdown-btn')) {
       document.getElementById('dropdown-container').style.display = "none";
