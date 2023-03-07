8d455946e6f2b076d75e5a1a7b455480e9fd5989
TomJegou <tomjegou10@gmail.com>
2023-03-07 10:07:39 +0100

    add methods for the struct libLocations

--- src/locationHandler.go
+++ src/locationHandler.go
@@ -1,11 +1,65 @@
 package src
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type LibLocations struct {
+	LocationsList map[string][]string
+	*ListenAddr
+}
+
+func (lib LibLocations) Locations() []string {
+	result := []string{}
+	for cityName := range lib.LocationsList {
+		result = append(result, cityName)
+	}
+	return result
+}
+
+func (lib LibLocations) InLocations(location string) bool {
+	for i := 0; i < len(lib.Locations()); i++ {
+		if location == lib.Locations()[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func NewLibLocations() *LibLocations {
+	locationList := make(map[string][]string)
+	return &LibLocations{LocationsList: locationList}
+}
+
+var libLocations = NewLibLocations()
+
+func getLocations() {
+	for i := 0; i < len(Relations["index"]); i++ {
+		for j := 0; j < len(Relations["index"][i].DatesLocations); j++ {
+			for cityName, listDate := range Relations["index"][i].DatesLocations {
+				if !libLocations.InLocations(cityName) {
+					libLocations.LocationsList[cityName] = listDate
+				} else {
+					libLocations.LocationsList[cityName] = append(libLocations.LocationsList[cityName], listDate...)
+				}
+			}
+		}
+	}
+}
 
 func locationHandler(w http.ResponseWriter, r *http.Request) {
 	ChangeListenAddr(r)
+	libLocations.ListenAddr = &ListeningAddr
 	OnLibraryArtists = false
-	go ParseHtml("static/html/location.html")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	PutBodyResponseApiIntoStruct(URLRELATION, &Relations, &wg)
+	wg.Wait()
+	getLocations()
+	fmt.Println(libLocations.LocationsList)
+	go ParseHtml("static/html/locations.html")
 	template := <-ChanTemplates
-	template.Execute(w, ListeningAddr)
+	template.Execute(w, libLocations)
 }
