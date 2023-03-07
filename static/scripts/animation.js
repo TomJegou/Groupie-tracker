5968446c7fd417318a15d3784727372ec08f4264
L0uxe <loulourxl@hotmail.com>
2023-03-07 21:51:07 +0100

    wip

--- static/scripts/animation.js
+++ static/scripts/animation.js
@@ -1,32 +1,15 @@
+document.addEventListener("click", e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
 
-// function toggleDropdown() {
-//   document.getElementById("dropdown").classList.toggle("show");
-// }
-// //open/close when dropdown button is clicked
-// document.getElementById("btn").addEventListener("click",function(){ toggleDropdown()});
-// Close dropdown when dom element is clicked
-// document.documentElement.addEventListener("click", function () {
-//   if (document.getElementsByClassName("dropdown").classList.classList("show")) {
-//     toggleDropdown();
-//   }
-
-// 
-//   // Get the button, and when the user clicks on it, execute myFunction
-//   document.getElementById("hamburger-icon").onclick = function() {myFunction()};
-
-//   /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-//   function myFunction() {
-//     document.getElementById("dropdown-content").classList.toggle("show");
-//   }
-// }
-window.onload = () => {
-  document.getElementById("btn").addEventListener("click", toggleDropdown)
-  // document.getElementById("btn").onclick = function() {toggleDropdown()};
-  function toggleDropdown() {
-    document.getElementById("dropdown").classList.toggle("show");
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
   }
-  // document.documentElement.addEventListener("click", function () {
-  //   if (document.getElementsByClassName("dropdown").classList.classList("show")) {
-  // toggleDropdown();
-      }
-// })}
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
+  })
+})
