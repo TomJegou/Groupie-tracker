1c240dfefdb3c65d57adb1a5c81d1490e6828a33
L0uxe <loulourxl@hotmail.com>
2023-03-11 15:03:03 +0100

    dropdown doesnt work

--- static/scripts/animation.js
+++ static/scripts/animation.js
@@ -1,52 +1,41 @@
 
-const dropdowns = document.querySelectorAll('.dropdown-toggle');
-dropdowns.forEach(dropdown => {
-  if (dropdown.classList.contains('click-dropdown')) {
-    dropdown.addEventListener('click', (event) => {
-      event.preventDefault();
-      const parent = dropdown.parentElement;
-      const dropdownMenu = parent.querySelector('.dropdown-menu');
-      const isOpen = dropdownMenu.classList.contains('dropdown-active');
+// const dropdowns = document.querySelectorAll('.dropdown-toggle');
 
-      closeDropdown();
+// dropdowns.forEach(dropdown => {
+//   if (dropdown.classList.contains('click-dropdown')) {
+//     dropdown.addEventListener('click', (event) => {
+//       event.preventDefault();
+//       console.log('click')
+//       const parent = dropdown.parentElement;
+//       const dropdownMenu = parent.querySelector('.dropdown-menu');
+//       const isOpen = dropdownMenu.classList.contains('dropdown-active');
 
-      if (!isOpen) {
-        parent.classList.add('dropdown-open');
-        dropdownMenu.classList.add('dropdown-active');
-      }
-    });
-  }
-});
+//       closeDropdown();
 
-dropdowns.forEach(dropdown => {
-  console.log('start')
-  if (dropdown.classList.contains('dropdown')) {
-    dropdown.addEventListener('click', (event) => {
-      const parent = dropdown.parentElement;
-      const dropdownMenu = parent.querySelector('.dropdown-menu');
-      parent.classList.add('dropdown-open');
-      dropdownMenu.classList.add('dropdown-active');
-    });
-    dropdown.addEventListener('click', (event) => {
-      const parent = dropdown.parentElement;
-      const dropdownMenu = parent.querySelector('.dropdown-menu');
-      dropdownMenu.addEventListener('click', closeDropdown);
-    });
-  }
-});
+//       if (!isOpen) {
+//         parent.classList.add('dropdown-open');
+//         dropdownMenu.classList.add('dropdown-active');
+//       }
+//     });
+//   }
+// });
 
-function closeDropdown() { 
-  document.querySelectorAll('.dropdown-open').forEach((openDropdown) => {
-    openDropdown.classList.remove('dropdown-open');
-  });
+// function closeDropdown() { 
+//   document.querySelectorAll('.dropdown-open').forEach((openDropdown) => {
+//     openDropdown.classList.remove('dropdown-open');
+//   });
 
-  document.querySelectorAll('.dropdown-active').forEach((activeDropdown) => {
-    activeDropdown.classList.remove('dropdown-active');
-  });
-}
-window.addEventListener('click', (event) => {
-  console.log('close')
-  if (!event.target.closest('.dropdown-container')) {
-    closeDropdown();
-  }
-});
+//   document.querySelectorAll('.dropdown-active').forEach((activeDropdown) => {
+//     activeDropdown.classList.remove('dropdown-active');
+//   });
+// }
+
+
+
+
+// window.addEventListener('click', (event) => {
+//   if (!event.target.closest('.dropdown-container')) {
+//     closeDropdown();
+//     console.log('close')
+//   }
+// });
