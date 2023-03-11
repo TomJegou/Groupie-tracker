4383e547a8e3a7f2d8f8e858bfa518fc3e4ac32a
TomJegou <tomjegou10@gmail.com>
2023-03-11 11:38:25 +0100

    regroup func into one file

--- src/api/herokuapp.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func MakeReqHerokuapp(url string) *http.Request {
-	return MakeReq(url, nil, nil)
-}
--- src/api/makeRequest.go
+++ src/api/makeRequest.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	gds "absolut-music/src/globalDataStructures"
+	"absolut-music/src/tools"
 	"fmt"
 	"net/http"
 	u "net/url"
 	"strings"
+	"sync"
 )
 
 func MakeReq(url string, headers map[string]string, forms map[string]string) *http.Request {
@@ -32,4 +35,23 @@ func MakeReq(url string, headers map[string]string, forms map[string]string) *ht
 		}
 		return req
 	}
+}
+
+func MakeReqHerokuapp(url string) *http.Request {
+	return MakeReq(url, nil, nil)
+}
+
+func MakeReqSearchAPISportify(artistName string) *http.Request {
+	url := "https://api.spotify.com/v1/search?q=" + tools.PreprocessArtNameSearchSpotify(artistName) + "&type=artist&offset=0&limit=1"
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go PutBodyResponseApiIntoStruct(RequestApi(MakeReqTokenSpotify()), &gds.OAuthSpotifyToken, &wg)
+	wg.Wait()
+	return MakeReq(url, map[string]string{"Authorization": "Bearer " + gds.OAuthSpotifyToken.Access_token}, nil)
+}
+
+func MakeReqTokenSpotify() *http.Request {
+	headers := map[string]string{"Authorization": "Basic " + gds.EncodedAuth, "Content-Type": "application/x-www-form-urlencoded"}
+	forms := map[string]string{"grant_type": "client_credentials"}
+	return MakeReq("https://accounts.spotify.com/api/token", headers, forms)
 }
