90de2b6646e10a0c456adb367ba7c092b6fe3a65
TomJegou <tomjegou10@gmail.com>
2023-03-12 17:00:25 +0100

    wip

--- static/scripts/script3d.js
+++ static/scripts/script3d.js
@@ -1,18 +1,20 @@
-import * as THREE from './node_modules/three/src/Three.js';
+import * as THREE from './node_modules/three/build/three.module.js';
+import * as gltf from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
-document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setPixelRatio(devicePixelRatio);
+document.getElementById("object3d").appendChild( renderer.domElement );
+
+const loader = new gltf.GLTFLoader();
+loader.load( "../img/casque.gltf", function (gltf) {
+    scene.add(gltf.scene)
+} );
 camera.position.z = 10;
-function animate() {
-    requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
-}
+// function animate() {
+//     requestAnimationFrame( animate );
+//     cube.rotation.y += 0.008;
+//     renderer.render( scene, camera );
+// }
 animate();
